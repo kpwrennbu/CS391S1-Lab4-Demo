7f089ee4d9ca5c0cfff27c774c5550ffc39f7015
kpwrennbu <kpwrenn@bu.edu>
2025-09-20 13:33:36 -0400

    removes code

--- src/components/DogContainer.jsx
+++ src/components/DogContainer.jsx
@@ -1,9 +1,9 @@
 
-export default function DogContainer(props) {
+export default function DogContainer(/*what goes here */) {
     return ( 
         <img
-            key={props.idx}
-            src={props.url}
+            key={/*what goes here */}
+            src={/*what goes here */}
             alt="Random Dog"
             style={{ width: "100%", borderRadius: "8px" }}
           />
